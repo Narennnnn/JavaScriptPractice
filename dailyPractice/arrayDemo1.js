cc116791bf63560dad23faa3a301c5d8c2de801d
Narendra Maurya <mauryanarendra2003@gmal.com>
2023-05-26 10:44:55 +0530

    commit changes

--- dailyPractice/arrayDemo1.js
+++ dailyPractice/arrayDemo1.js
@@ -1,5 +1,8 @@
 const arr = [100, "Harsh", 68.50]
-for (var x of arr)//element
-    console.log(x);
-for (var x in arr)//indices
-    console.log(arr[x]);
+// for (var x of arr)//element
+//     console.log(x);
+// for (var x in arr)//indices
+//     console.log(arr[x]);
+arr.forEach(function (element, index, array) {//anaonymous function
+    console.log(element + " index: " + index + " array: " + array);
+});
---
+++ dailyPractice/practiceArray.js
@@ -0,0 +1,10 @@
+const myCar = ['Lambo', 'honda', 'maruti', 'WagonR']
+function myFunction() {
+    console.log("Just checking!")
+}
+const person = { firstName: "John", lastName: "Doe", age: 46, salary: 45000.50, gender: 'm' };
+const ArrayKing = new Array
+ArrayKing[0] = myCar
+ArrayKing[1] = myFunction
+ArrayKing[2] = person
+console.log(ArrayKing[2])//You can have objects in an Array. You can have functions in an Array. You can have arrays in an Array:
